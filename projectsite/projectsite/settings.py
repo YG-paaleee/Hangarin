a644975454ef90d6f6a6e83ec29dd816fe91b445
Ralph Angelo Badang <202380099@psu.palawan.edu.ph>
2025-09-04 11:30:37 +0800

    mody allowed host

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9jl+e2m(pmec9*a=g43!@yy!z6flsx-dzx*nd9g77rsn(nq1b#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://paaleee3.pythonanywhere.com/',]
 
 
 # Application definition
