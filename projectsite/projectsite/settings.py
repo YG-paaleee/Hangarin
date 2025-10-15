58f815f9822e01bbfe08d68ccfb4d488d5d6aea0
YG-paaleee <202380099@psu.palawan.edu.ph>
2025-10-16 05:46:18 +0800

    added github auth

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import socket
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-9jl+e2m(pmec9*a=g43!@yy!z6flsx-dzx*nd9g77rsn(nq1b#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['paaleee3.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['paaleee3.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -49,7 +50,11 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.github',
 ]
 
-SITE_ID = 1
+if "pythonanywhere" in socket.gethostname():
+    SITE_ID = 2 # production site (psusphere.pythonanywhere.com)
+else:
+    SITE_ID = 1 # local site (127.0.0.1:8000)
+
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
