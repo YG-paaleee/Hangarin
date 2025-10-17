a3d930f5b85d880fe94274068e18c01d32dd16b5
YG-paaleee <202380099@psu.palawan.edu.ph>
2025-10-17 12:32:32 +0800

    fixed pwa logo not showing up

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -172,29 +172,69 @@ PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
-        'src': '/static/img/undraw_rocket.svg',
-        'sizes': '192x192',
-        'type': 'image/svg+xml',
+        'src': '/static/img/icons/icon-72x72.png',
+        'sizes': '72x72',
+        'type': 'image/png',
         'purpose': 'any maskable'
     },
     {
-        'src': '/static/img/undraw_rocket.svg',
+        'src': '/static/img/icons/icon-96x96.png',
+        'sizes': '96x96',
+        'type': 'image/png',
+        'purpose': 'any maskable'
+    },
+    {
+        'src': '/static/img/icons/icon-128x128.png',
+        'sizes': '128x128',
+        'type': 'image/png',
+        'purpose': 'any maskable'
+    },
+    {
+        'src': '/static/img/icons/icon-144x144.png',
+        'sizes': '144x144',
+        'type': 'image/png',
+        'purpose': 'any maskable'
+    },
+    {
+        'src': '/static/img/icons/icon-152x152.png',
+        'sizes': '152x152',
+        'type': 'image/png',
+        'purpose': 'any maskable'
+    },
+    {
+        'src': '/static/img/icons/icon-192x192.png',
+        'sizes': '192x192',
+        'type': 'image/png',
+        'purpose': 'any maskable'
+    },
+    {
+        'src': '/static/img/icons/icon-384x384.png',
+        'sizes': '384x384',
+        'type': 'image/png',
+        'purpose': 'any maskable'
+    },
+    {
+        'src': '/static/img/icons/icon-512x512.png',
         'sizes': '512x512',
-        'type': 'image/svg+xml',
+        'type': 'image/png',
         'purpose': 'any maskable'
     }
 ]
 PWA_APP_ICONS_APPLE = [
     {
-        'src': '/static/img/undraw_rocket.svg',
-        'sizes': '180x180'
+        'src': '/static/img/icons/icon-152x152.png',
+        'sizes': '152x152'
+    },
+    {
+        'src': '/static/img/icons/icon-192x192.png',
+        'sizes': '192x192'
     }
 ]
 PWA_APP_DIR = 'ltr'
 PWA_APP_LANG = 'en-US'
 PWA_APP_SPLASH_SCREEN = [
     {
-        'src': '/static/img/undraw_rocket.svg',
+        'src': '/static/img/icons/icon-512x512.png',
         'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
     }
 ]
